e82b62a521562264ed87acd063890b2f94b7af7a
LuanPinheiro <luanp.soledade@gmail.com>
2022-10-08 18:58:31 -0300

    Melhora na legibilidade dos outputs

--- projeto_1/projeto1.js
+++ projeto_1/projeto1.js
@@ -1,9 +1,12 @@
-let a = 'vermelho'
-let b = 'azul'
+let a = 'abc'
+let b = 15
+
+console.log('Valor da primeira variavel:' + ' ' + a)
+console.log('Valor da segunda variavel:' + ' ' + b)
 
 let c = a
 a = b
 b = c
 
-console.log(a)
-console.log(b)
+console.log('Primeira Variavel: ', a)
+console.log('Segunda Variavel: ', b)
