43cc3b442fff69d0258175d8b4bbce9561b9ebba
LuanPinheiro <luanp.soledade@gmail.com>
2022-10-10 01:38:47 -0300

    Projeto 2 Concluído

---
+++ projeto_2/README.md
@@ -0,0 +1,2 @@
+# Projeto 2
+Função que recebe 2 números e retorna o maior entre eles
---
+++ projeto_2/projeto2.js
@@ -0,0 +1,7 @@
+function maior(num1,num2){
+    return num1>num2 ? num1:num2
+}
+
+let num1 = 12, num2 = 6
+console.log(`Entrada: ${num1} e ${num2}`)
+console.log(`Maior numero: ${maior(num1, num2)}`)
